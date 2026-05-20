ed2980af1f8dd253340d247f557c60ca875545b0
Hugo-M-R <hugomr292006@gmail.com>
2026-05-20 17:39:05 -0300

    remove Docker configuration and update Vite proxy target to localhost

--- JS/vite.config.js
+++ JS/vite.config.js
@@ -2,16 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-const proxyApiTarget =
-  process.env.VITE_PROXY_API_TARGET || 'http://localhost:3000';
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/auth': {
-        target: proxyApiTarget,
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
