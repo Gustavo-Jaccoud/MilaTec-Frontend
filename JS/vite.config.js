5aabd95495f9d18203665860fb38229e87be8737
Gustavo Jaccoud <gustavo.c.jaccoud@gmail.com>
2026-06-09 12:21:19 -0300

    fix: remove auth proxy

--- JS/vite.config.js
+++ JS/vite.config.js
@@ -2,17 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/auth': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
